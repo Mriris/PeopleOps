a6c369a463d01b3fcb8a59b9873357e7a934294b
Mriris <2647381485@qq.com>
2024-10-10 22:42:21 +0800

    员工列表前后端连接

--- frontend/src/components/gly/ygList.vue
+++ frontend/src/components/gly/ygList.vue
@@ -38,20 +38,27 @@ export default {
   methods: {
     getTableData () {
       // 调用后端接口，从数据库中取出员工列表
-      this.total = 6; // 表示总的数据条数是6
-      if (this.currentPage == 1) {
-        this.tableData = [
-          {id:'e235cc4e882f4cf692a393890aeed28b',bh:'YG0001',xm:'刘备',xb:'男',rzrq:'2021-01-01',bm:'总经理办公室',gw:'总经理'},
-          {id:'3ec0d5c612fa46da963c62cd21a53097',bh:'YG0002',xm:'关羽',xb:'男',rzrq:'2021-01-01',bm:'人事行政部',gw:'人事行政经理'},
-          {id:'5c9d48960136440b97e518377d9b7e0f',bh:'YG0003',xm:'张飞',xb:'男',rzrq:'2021-01-01',bm:'人事行政部',gw:'人事主管'}
-        ]
-      } else {
-        this.tableData = [
-          {id:'0ba6b693d1fd4fd1a9223645c48c61ed',bh:'YG0004',xm:'马超',xb:'男',rzrq:'2021-01-01',bm:'人事行政部',gw:'人事专员'},
-          {id:'d8238c31965f4ced9c827e55cd0d8bf0',bh:'YG0005',xm:'廖化',xb:'男',rzrq:'2021-01-01',bm:'人事行政部',gw:'人事专员'},
-          {id:'836f5e22ca7d4e2294abbacfaf2f789e',bh:'YG0006',xm:'魏延',xb:'男',rzrq:'2021-01-01',bm:'人事行政部',gw:'人事专员'}
-        ];
-      }
+      const formData = new FormData();
+      formData.append('page', this.currentPage);
+      formData.append('pageSize', this.pageSize);
+      this.axios.post('/backend/ygList', formData).then(response => {
+        this.total = response.data.total;
+        this.tableData = response.data.list;
+      });
+      // this.total = 6; // 表示总的数据条数是6
+      // if (this.currentPage == 1) {
+      //   this.tableData = [
+      //     {id:'e235cc4e882f4cf692a393890aeed28b',bh:'YG0001',xm:'刘备',xb:'男',rzrq:'2021-01-01',bm:'总经理办公室',gw:'总经理'},
+      //     {id:'3ec0d5c612fa46da963c62cd21a53097',bh:'YG0002',xm:'关羽',xb:'男',rzrq:'2021-01-01',bm:'人事行政部',gw:'人事行政经理'},
+      //     {id:'5c9d48960136440b97e518377d9b7e0f',bh:'YG0003',xm:'张飞',xb:'男',rzrq:'2021-01-01',bm:'人事行政部',gw:'人事主管'}
+      //   ]
+      // } else {
+      //   this.tableData = [
+      //     {id:'0ba6b693d1fd4fd1a9223645c48c61ed',bh:'YG0004',xm:'马超',xb:'男',rzrq:'2021-01-01',bm:'人事行政部',gw:'人事专员'},
+      //     {id:'d8238c31965f4ced9c827e55cd0d8bf0',bh:'YG0005',xm:'廖化',xb:'男',rzrq:'2021-01-01',bm:'人事行政部',gw:'人事专员'},
+      //     {id:'836f5e22ca7d4e2294abbacfaf2f789e',bh:'YG0006',xm:'魏延',xb:'男',rzrq:'2021-01-01',bm:'人事行政部',gw:'人事专员'}
+      //   ];
+      // }
     },
     handleView (id) {
       this.$router.push({ name: 'ygDetail', params: { id: id } }); // 跳转到员工详细页面
