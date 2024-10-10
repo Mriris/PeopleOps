5cc8b1b72ce45adc7ba8e705cf060a1b745d9285
Mriris <2647381485@qq.com>
2024-10-10 17:44:59 +0800

    样例

--- frontend/src/components/layout/aside.vue
+++ frontend/src/components/layout/aside.vue
@@ -4,13 +4,20 @@
       <el-menu-item style="text-align: left;" index="/welcome"><i class="el-icon-s-home"></i>首页</el-menu-item>
     </el-menu-item-group>
     <el-submenu index="1">
+      <template slot="title"><i class="el-icon-menu"></i>部门管理</template>
+      <el-menu-item-group>
+        <el-menu-item index="/bmBuild">新建部门</el-menu-item>
+        <el-menu-item index="/bmList">部门列表</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+    <el-submenu index="2">
       <template slot="title"><i class="el-icon-menu"></i>员工管理</template>
       <el-menu-item-group>
         <el-menu-item index="/ygBuild">新建员工</el-menu-item>
         <el-menu-item index="/ygList">员工列表</el-menu-item>
       </el-menu-item-group>
     </el-submenu>
-    <el-submenu index="2">
+    <el-submenu index="3">
       <template slot="title"><i class="el-icon-s-tools"></i>账户管理</template>
       <el-menu-item-group>
         <el-menu-item index="/changePassword">修改密码</el-menu-item>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,6 +11,11 @@ import ygDetail from '@/components/gly/ygDetail'
 import ygBuild from '@/components/gly/ygBuild'
 import ygUpdate from '@/components/gly/ygUpdate'
 
+import bmList from '@/components/gly/bmList'
+import bmDetail from '@/components/gly/bmDetail'
+import bmBuild from '@/components/gly/bmBuild'
+import bmUpdate from '@/components/gly/bmUpdate'
+
 import changePassword from '@/components/gly/changePassword'
 
 Vue.use(Router);
@@ -61,6 +66,30 @@ export default new Router({
           path: '/ygUpdate',
           name: 'ygUpdate',
           component: ygUpdate,
+          meta: { auth: '1' }
+        },
+        {
+          path: '/bmList',
+          name: 'bmList',
+          component: bmList,
+          meta: { auth: '1' }
+        },
+        {
+          path: '/bmDetail',
+          name: 'bmDetail',
+          component: bmDetail,
+          meta: { auth: '1' }
+        },
+        {
+          path: '/bmBuild',
+          name: 'bmBuild',
+          component: bmBuild,
+          meta: { auth: '1' }
+        },
+        {
+          path: '/bmUpdate',
+          name: 'bmUpdate',
+          component: bmUpdate,
           meta: { auth: '1' }
         },
         {
